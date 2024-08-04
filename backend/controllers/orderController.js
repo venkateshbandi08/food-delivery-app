555c3941fea4989a34571d7e1db8cac1b4e51bbc
venkatesh bandi <100306737+venkateshbandi08@users.noreply.github.com>
2024-08-04 20:05:22 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order from frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://big-bytes.vercel.app";
+  const frontend_url = "https://localhost:4000";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
